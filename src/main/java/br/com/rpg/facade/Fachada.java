5fea311b73cb8b84f2db5ca9602f6dc218eda71e
Ivieri <ivieriramos@gmail.com>
2026-02-13 18:27:31 -0300

    iniciando o projeto

---
+++ src/main/java/br/com/rpg/facade/Fachada.java
@@ -0,0 +1,6 @@
+package br.com.rpg.facade;
+
+public class Fachada {
+    // AQUI SERA IMPLEMENTADA O QUE IRA CHAMAR O CONTROLLER E
+    // RECEBER AS INFORMACOES DE VIEW
+}
---
+++ src/main/java/br/com/rpg/model/entities/Personagem.java
@@ -0,0 +1,6 @@
+package br.com.rpg.model.entities;
+
+public abstract class Personagem {
+    // AQUI SERA IMPLEMENTADA OS ATRIBUTOS BASE DE
+    // INIMIGOS E HEROIS
+}
