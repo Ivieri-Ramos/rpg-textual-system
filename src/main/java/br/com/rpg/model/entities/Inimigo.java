e518ebc81075c57dca06bc5d97bdb234e3baf94f
una-claire <kalissa32355@proton.me>
2026-02-15 11:52:41 -0300

    criando Classe Inimigo e Enum ClasseInimigo

--- src/main/java/br/com/rpg/model/entities/Inimigo.java
+++ src/main/java/br/com/rpg/model/entities/Inimigo.java
@@ -1,4 +1,31 @@
 package br.com.rpg.model.entities;
 
-public class Inimigo{
+import br.com.rpg.model.enums.ClasseInimigo;
+
+/**
+ * Classe que será usada para criar inimigos no jogo.
+ * <p>
+ */
+public class Inimigo extends Personagem {
+
+    /**
+     * Construtor especializado de Inimigo que define
+     * atributos-base a partir de tipos de classes pré-definidas.
+     * <p>
+     * Utiliza o {@link ClasseInimigo} a partir das suas constantes Enum
+     * e define os atributos-base (vida, dano, chanceCrit, etc.) usados pelo construtor
+     * em {@link Personagem}, para criar a entidade Inimigo.
+     * @param tipoClasse Tipos de classes pré-definidas ({@code GOBLIN}, {@code ORC}, etc.) que atribui valores base.
+     * @param nome Nome identificador do inimigo.
+     */
+    public Inimigo(ClasseInimigo tipoClasse, String nome) {
+        super(tipoClasse.getVidaBase(), tipoClasse.getDanoBase(), tipoClasse.getManaBase(),
+                tipoClasse.getDefesaBase(), tipoClasse.getChanceCritBase(),
+                tipoClasse.getChanceEsqBase(), nome);
+    }
+
+    @Override
+    public String toString() {
+        return "Inimigo{" + super.toString() + '}';
+    }
 }
--- src/main/java/br/com/rpg/model/enums/ClasseInimigo.java
+++ src/main/java/br/com/rpg/model/enums/ClasseInimigo.java
@@ -1,4 +1,44 @@
 package br.com.rpg.model.enums;
 
-public class ClasseInimigo {
+public enum ClasseInimigo {
+    GOBLIN(30, 8, 0, 5.0, 10.0, 25.0),
+
+    ORC(80, 15, 0, 20.0, 5.0, 5.0);
+
+    private final int vidaBase;
+    private final int danoBase;
+    private final int manaBase;
+    private final double defesaBase;
+    private final double chanceCritBase;
+    private final double chanceEsqBase;
+
+    ClasseInimigo(int vidaBase, int danoBase, int manaBase, double defesaBase, double chanceCritBase, double chanceEsqBase) {
+
+        this.vidaBase = vidaBase;
+        this.danoBase = danoBase;
+        this.manaBase = manaBase;
+        this.defesaBase = defesaBase;
+        this.chanceCritBase = chanceCritBase;
+        this.chanceEsqBase = chanceEsqBase;
+    }
+
+    public int getVidaBase() {
+        return vidaBase;
+    }
+    public int getDanoBase() {
+        return danoBase;
+    }
+    public int getManaBase() {
+        return manaBase;
+    }
+    public double getDefesaBase() {
+        return defesaBase;
+    }
+    public double getChanceCritBase() {
+        return chanceCritBase;
+    }
+    public double getChanceEsqBase() {
+        return chanceEsqBase;
+    }
+
 }
