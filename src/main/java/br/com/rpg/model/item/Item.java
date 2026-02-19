93bfc6b4e4b93d2ecff6e757e0cfcb99831553a1
Ivieri <ivieriramos@gmail.com>
2026-02-19 11:37:44 -0300

    Implementado a classe abstrata Item

---
+++ src/main/java/br/com/rpg/model/item/Item.java
@@ -0,0 +1,36 @@
+package br.com.rpg.model.item;
+
+/**
+ * Armazena os atributos gerais das classes Equipamento e
+ * Consumivel que herdarão dela.
+ */
+public abstract class Item {
+
+    private final String nome;
+    private final String descricao;
+    private final int preco;
+
+    /**
+     * Construtor padrão da classe, apenas instancia os atributos base.
+     * @param nome Identificação única.
+     * @param descricao Detalhe o propósito do item.
+     * @param preco Custo do item, será necessário posteriormente para um sistema de loja.
+     */
+    public Item(String nome, String descricao, int preco) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public int getPreco() {
+        return preco;
+    }
+}
