9cef426e0c2fb097c4b684d2e77e9250f6b25f52
SrTartaruga <franciscocaetes@gmail.com>
2026-02-23 12:30:01 -0300

    Pequenas correços de nos atributos dos equipamentos e na versão do gradel

--- src/main/java/br/com/rpg/exceptions/EquipamentoNaoEncontradoException.java
+++ src/main/java/br/com/rpg/exceptions/EquipamentoNaoEncontradoException.java
@@ -1,7 +1,5 @@
 package br.com.rpg.exceptions;
 
-import br.com.rpg.model.entities.CatalogoEquipamentos;
-
 public class EquipamentoNaoEncontradoException extends RuntimeException {
     public EquipamentoNaoEncontradoException(String nomeEquipamento) {
         super("Equipamento de nome: " + nomeEquipamento + " não foi encontrada no Catálogo existente!\nVerifique novamente.");
--- src/main/java/br/com/rpg/model/item/CatalogoEquipamentos.java
+++ src/main/java/br/com/rpg/model/item/CatalogoEquipamentos.java
@@ -1,8 +1,7 @@
-package br.com.rpg.model.entities;
+package br.com.rpg.model.item;
 
 import br.com.rpg.exceptions.EquipamentoNaoEncontradoException;
 import br.com.rpg.model.enums.TipoEquipamento;
-import br.com.rpg.model.item.Equipamentos;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,9 +22,9 @@ public final class CatalogoEquipamentos {
                 0,
                 0,
                 0,
-                5,
-                0,
-                10));
+                5.0,
+                0.0,
+                10.0));
         mapaEquipamentos.put("ARMADURA COMPLETA", new Equipamentos(
                 "Armadura Completa",
                 "Armadura pesada que com muita defesa",
@@ -34,53 +33,53 @@ public final class CatalogoEquipamentos {
                 0,
                 0,
                 0,
-                15,
-                0,
-                -5));
+                15.0,
+                0.0,
+                -5.0));
         mapaEquipamentos.put("ESPADA CURTA", new Equipamentos(
                 "Espada Curta",
                 "Espada simples",
                 1,
-                TipoEquipamento.MAOPRINCIPAL,
+                TipoEquipamento.MAO_PRINCIPAL,
                 0,
                 5,
                 0,
-                0,
-                0,
-                0));
+                0.0,
+                0.0,
+                0.0));
         mapaEquipamentos.put("MACHADO GRANDE", new Equipamentos(
                 "Machado Grande",
                 "Poderoso Machado com alto dano, reduz a esquiva",
                 10,
-                TipoEquipamento.MAOPRINCIPAL,
+                TipoEquipamento.MAO_PRINCIPAL,
                 0,
                 25,
                 0,
-                0,
-                0,
-                -10));
+                0.0,
+                0.0,
+                -10.0));
         mapaEquipamentos.put("ESCUDO DE APARAR", new Equipamentos(
                 "Escudo de Aparar",
                 "Pequeno escudo usado para aparar golpe. Aumenta um pouco da armadura e esquiva",
                 5,
-                TipoEquipamento.MAOSEGUNDARIA,
+                TipoEquipamento.MAO_SEGUNDARIA,
                 0,
                 0,
                 0,
-                5,
-                0,
+                5.0,
+                0.0,
                 7.5));
         mapaEquipamentos.put("ESCUDO GRANDE", new Equipamentos(
                 "Escudo Grande",
                 "Escudo grande o suficiente para bloquear todos os tipos de golpe. Reduz a esquiva",
                 5,
-                TipoEquipamento.MAOSEGUNDARIA,
+                TipoEquipamento.MAO_SEGUNDARIA,
                 0,
                 0,
                 0,
-                25,
-                0,
-                -5));
+                25.0,
+                0.0,
+                -5.0));
         mapaEquipamentos.put("MANTO DA SAÚDE", new Equipamentos(
                 "Manto da Saúde",
                 "Esse manto foi imbuído em poções de cura!",
@@ -89,9 +88,9 @@ public final class CatalogoEquipamentos {
                 100,
                 0,
                 20,
-                0,
-                0,
-                0));
+                0.0,
+                0.0,
+                0.0));
         mapaEquipamentos.put("CAPA ELUSIVA", new Equipamentos(
                 "Capa elusiva",
                 "Quando equipado, deixa o usuário praticamente invisível, mas ao custo sua vida",
@@ -100,9 +99,9 @@ public final class CatalogoEquipamentos {
                 -50,
                 0,
                 0,
-                0,
-                0,
-                50));
+                0.0,
+                0.0,
+                50.0));
     }
 
     public static Equipamentos enviarEquipamento(String chave){
