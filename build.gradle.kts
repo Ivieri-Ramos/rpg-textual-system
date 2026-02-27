937f3dadc004aeed672aee84004180d689cb7bd3
Ivieri Ramos <ivieriramos@gmail.com>
2026-02-27 18:15:02 -0300

    Adicionado dependência com jackson no build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
 }
 
 tasks.test {
