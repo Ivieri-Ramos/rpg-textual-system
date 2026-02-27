5a018be408cde5169f0d99c5e86d23601cfb6ce8
Ivieri Ramos <ivieriramos@gmail.com>
2026-02-27 20:34:51 -0300

    Criado CatalogoHeroi que substitui ClasseHeroi, centralizando a ideia de pedir um novo herói

--- src/main/java/br/com/rpg/Main.java
+++ src/main/java/br/com/rpg/Main.java
@@ -1,7 +1,5 @@
 package br.com.rpg;
 
-import br.com.rpg.model.entities.heroi.Heroi;
-import br.com.rpg.model.entities.heroi.ClasseHeroi;
 import br.com.rpg.model.mundo.MundoBuilder;
 import br.com.rpg.view.Teclado;
 
@@ -12,7 +10,6 @@ public class Main {
         try {
             // Esse bloco de inicializações é apenas para testes, será futuramente retirado isso.
             MundoBuilder construtor = new MundoBuilder();
-            Heroi meuHeroi = new Heroi("Ivieri", ClasseHeroi.GUERREIRO);
             // meuInimigo.aprenderHabilidade(CatalogoHabilidades.enviarHabilidade("ATAQUE_FORTE"));
             // Cidade cidadeInicial = construtor.gerarCidadePrincipal();
             // ExploracaoController jogo = new ExploracaoController(cidadeInicial);
--- src/main/java/br/com/rpg/model/entities/heroi/Heroi.java
+++ src/main/java/br/com/rpg/model/entities/heroi/Heroi.java
@@ -2,6 +2,8 @@ package br.com.rpg.model.entities.heroi;
 
 import br.com.rpg.model.entities.Personagem;
 
+import java.util.List;
+
 /**
      * Classe que será usada pelo jogador para controlar o fluxo do jogo.
      * <p>
@@ -13,17 +15,20 @@ public class Heroi extends Personagem {
      * Construtor especializado de Heroi que define
      * atributos-base a partir de tipos de classes pré-definidas.
      * <p>
-     * Utiliza a {@link ClasseHeroi} a partir das suas constantes Enum
-     * e define os atributos-base (vida, dano, chanceCrit, etc.) usados pelo construtor
+     * Define os atributos-base (vida, dano, chanceCrit, etc.) usados pelo construtor
      * em {@link Personagem}, para criar a entidade Heroi.
      * @param nome Identificação do jogador única (não pode ser {@code null}).
-     * @param tipoClasse Tipos de classes pré-definidas ({@code Guerreiro}, {@code Mago}, etc.) que atribui valores base.
+     * @param vida
+     * @param dano
+     * @param mana
+     * @param defesa
+     * @param chanceCrit
+     * @param chanceEsq
+     * @param chaveHabilidades
      */
-    public Heroi(String nome, ClasseHeroi tipoClasse) {
-        super(nome, tipoClasse.getVidaBase(), tipoClasse.getDanoBase(),
-                tipoClasse.getManaBase(), tipoClasse.getDefesaBase(),
-                tipoClasse.getChanceCritBase(), tipoClasse.getChanceEsqBase(),
-                tipoClasse.getChaveHabilidades());
+    public Heroi(String nome, int vida, int dano, int mana, double defesa, double chanceCrit,
+                 double chanceEsq, List<String> chaveHabilidades) {
+        super(nome, vida, dano, mana, defesa, chanceCrit, chanceEsq, chaveHabilidades);
     }
 
     @Override
