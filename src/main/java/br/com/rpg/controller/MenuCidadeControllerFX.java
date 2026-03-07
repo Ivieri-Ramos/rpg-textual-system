15982824d1c13df51bb6549b124d521ede21fa42
Ivieri <ivieriramos@gmail.com>
2026-03-07 00:22:18 -0300

    Adicionado Menu Masmorras

--- src/main/java/br/com/rpg/controller/MenuCidadeControllerFX.java
+++ src/main/java/br/com/rpg/controller/MenuCidadeControllerFX.java
@@ -4,6 +4,7 @@ import br.com.rpg.model.core.SessaoJogo;
 import br.com.rpg.model.entities.heroi.Heroi;
 import br.com.rpg.model.mundo.Cidade;
 import br.com.rpg.model.services.SaveService;
+import br.com.rpg.view.GerenciadorTela;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -27,7 +28,7 @@ public class MenuCidadeControllerFX {
 
     @FXML
     private void onBotaoMenuMasmorrasClick() {
-        // TODO: Ir para o menu masmorras
+        GerenciadorTela.trocarTela("MenuMasmorras");
     }
 
     @FXML
--- src/main/java/br/com/rpg/controller/MenuMasmorrasControllerFX.java
+++ src/main/java/br/com/rpg/controller/MenuMasmorrasControllerFX.java
@@ -1,4 +1,38 @@
 package br.com.rpg.controller;
 
+import br.com.rpg.model.core.SessaoJogo;
+import br.com.rpg.model.mundo.Masmorra;
+import br.com.rpg.view.GerenciadorTela;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+
+import java.util.List;
+
 public class MenuMasmorrasControllerFX {
+
+    @FXML
+    private VBox caixaMasmorrasVBox;
+
+    @FXML
+    private void initialize() {
+        List<Masmorra> masmorras = SessaoJogo.getInstancia().getCidadeJogo().getMasmorrasProximas();
+        for (Masmorra masmorra : masmorras) {
+            Button novoButton = new Button();
+            novoButton.setText(masmorra.getNome());
+            novoButton.setPrefWidth(200);
+            novoButton.setPrefHeight(40);
+            novoButton.setFont(Font.font("Berry Rotunda", 14));
+            novoButton.setOnAction(e -> {
+               // TODO: Controller que sirva para cada tipo de Masmorra
+            });
+            this.caixaMasmorrasVBox.getChildren().add(novoButton);
+        }
+    }
+
+    @FXML
+    private void onBotaoVoltarClick() {
+        GerenciadorTela.trocarTela("MenuCidade");
+    }
 }
